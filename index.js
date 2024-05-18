761dd7f3267dca99d01f5941ef4a9224445def33
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-05-18 23:15:27 +0600

    Share Folders  :tshirt:

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use("/api/file", require("./modules/files/file.router"));
 // Folders
 app.use("/api/folder", require("./modules/folders/folder.route"));
 
-// share
+// Share
 
 app.use('/api/share', require('./modules/share/share.route'))
 
